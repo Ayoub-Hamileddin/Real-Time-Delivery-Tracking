6f6efcf657b6015b1877ea3576914eb1495f43b1
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-04-30 22:40:42 +0100

    feat(auth): add password reset endpoints and controller methods

--- backend/app/Http/Controllers/Auth/AuthController.php
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers\Auth;
 
 use App\Dto\Auth\LoginDto;
 use App\Dto\Auth\RegisterDto;
+use App\Dto\Auth\ResetlinkDto;
+use App\Dto\Auth\ResetPasswordDto;
 use App\Helper\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\AuthLogin;
 use App\Http\Requests\Auth\AuthRegister;
+use App\Http\Requests\ResetlinkRequest;
+use App\Http\Requests\ResetPasswordRequest;
 use App\Http\Resources\UserRessource;
 use App\Services\AuthService;
+use Backend\App\Helper\AuthHelper;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -23,6 +28,8 @@ class AuthController extends Controller
 
     public function register(AuthRegister $request){
         try {
+             AuthHelper::rateLimiting($request);
+
             $dto = RegisterDto::fromRequest($request);
             $response = $this->authService->authRegister($dto);
 
@@ -36,8 +43,12 @@ class AuthController extends Controller
 
     public function login(AuthLogin $request){
         try {
+
+            AuthHelper::rateLimiting($request);
+
             $dto = LoginDto::fromRequest($request);
             $response = $this->authService->authLogin($dto);
+
             return ApiResponse::success("login successfuly",$response,"success",200);
         } catch (\Throwable $e) {
             Log::error("Error : something wrong in logging ".$e->getMessage());
@@ -48,5 +59,46 @@ class AuthController extends Controller
     public function logout(Request $request){
         $this->authService->authLogout($request);
         return ApiResponse::success("Logout successfuly",null,"success",204);
+    }
+
+
+    public function verifiedEmail(Request $request){
+        try {
+            $this->authService->verifiedEmail($request);
+            return ApiResponse::success("Email verified successfuly");
+        } catch (\Throwable $e) {
+            Log::error("Error : verifiying email ".$e->getMessage());
+            return ApiResponse::error("Error while verifiying email","error",500);
+        }
+    }
+
+    public function resendEmail(Request $request){
+        try {
+            $this->authService->resendEmail($request);
+            return ApiResponse::success("Verification email sent");
+        } catch (\Throwable $e) {
+            Log::error("Error : verifiying email ".$e->getMessage());
+            return ApiResponse::error("Error while sending email","error",500);
+        }
+    }
+    public function forgetPassword(ResetlinkRequest $request){
+        try {
+            $dto = ResetlinkDto::fromRequest($request);
+            $this->authService->forgetPasswordLink($dto);
+            return ApiResponse::success("Reset password link sent successfuly");
+        } catch (\Throwable $e) {
+            Log::error("Error : resent link  ".$e->getMessage());
+            return ApiResponse::error("Error while sending resent link","error",500);
+        }
+    }
+    public function resetPassword(ResetPasswordRequest $request){
+        try {
+            $dto = ResetPasswordDto::fromRequest($request);
+            $this->authService->resetPassword($dto);
+            return ApiResponse::success("password updated successfuly");
+        } catch (\Throwable $e) {
+            Log::error("Error : updating password  ".$e->getMessage());
+            return ApiResponse::error("Error while updating password ","error",500);
+        }
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,10 +7,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function(){
     Route::prefix("/auth")->name("auth.")->group(function(){
-        Route::post("register","register")->name("register");
-        Route::post("login","login")->name("login");
-        Route::post("logout","logout")
-            ->middleware("auth:api")
-            ->name("logout");
+        // Guset auth endpoints
+        Route::middleware("guest")->group(function(){
+
+            Route::post("register","register")
+                ->name("register");
+
+            Route::post("login","login")
+                ->name("login");
+
+            Route::post("/forget-password","forgetPassword")
+                ->name("password.request");
+        });
+
+        Route::middleware("auth:api")->group(function(){
+
+            Route::post("logout","logout")
+                ->name("logout");
+
+            Route::post("/email/verify/{id}/{hash}","verifiedEmail")
+                ->name("verificationEmail");
+
+            Route::post('/email/resend',"resendEmail");
+
+            Route::post("reset-password","resetPassword")
+                ->name("password.update");
+        });
+
     });
 });
